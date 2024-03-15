7155ae42cb22c3914399a1d32c24838467f70599
Cynosure24 <139391583+Cynosure24@users.noreply.github.com>
2024-03-15 18:18:29 +0100

    Update NotFound.jsx

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import css from './NotFound.module.css';
-import { TbError404 } from 'react-icons/tb';
+import { Error404 } from 'react-icons/tb';
 import { Helmet } from 'react-helmet-async';
 
 export default function NotFound() {
   return (
     <div className={css.container}>
-      <TbError404 size={150} />
+      <Error404 size={150} />
       <p className={css.text}>Ops, not found page!</p>
       <NavLink to="/" className={css.link}>
         Go to the home page...
